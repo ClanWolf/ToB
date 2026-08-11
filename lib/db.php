c7678f556f16b0caa45edf177c557b323e72f522
WarWolfen <warwolfen@gmail.com>
2026-08-11 18:02:16 +0200

    Added global for the vars to be visible.

--- lib/db.php
+++ lib/db.php
@@ -4,12 +4,14 @@ require_once __DIR__ . '/db_credentials.php';
 
 function db(): PDO
 {
+	global $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS;
+
 	static $pdo = null;
 	if ($pdo === null) {
-		$host = $DB_HOST ?: 'db';
-		$name = $DB_NAME ?: 'cwg';
-		$user = $DB_USER ?: 'cwg';
-		$pass = $DB_PASS ?: 'cwg';
+		$host = $DB_HOST ?: '127.0.0.1';
+		$name = $DB_NAME ?: 'db';
+		$user = $DB_USER ?: 'user';
+		$pass = $DB_PASS ?: 'pass';
 		$pdo = new PDO(
 			"mysql:host={$host};dbname={$name};charset=utf8mb4",
 			$user,
