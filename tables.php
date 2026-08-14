a15d39dc8a314fc019658db752534bd3db300360
WarWolfen <warwolfen@gmail.com>
2026-08-14 15:21:48 +0200

    Fixed link to challonge

--- tables.php
+++ tables.php
@@ -58,7 +58,7 @@ $lang = require __DIR__ . '/lib/init.php';
 		</div>
 
 		<div class="container">
-			<iframe src="http://cwg.challonge.com/ToB_CW_B_201704/module?theme=3821&multiplier=1.0&match_width_multiplier=0.8&show_final_result=0"
+			<iframe src="https://cwg.challonge.com/ToB_CW_B_201704/module?theme=3821&multiplier=1.0&match_width_multiplier=0.8&show_final_result=0"
 				width="100%"
 				height="95%"
 				frameborder="0"
